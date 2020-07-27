f2cef52283fd623db0bf37a25d5d6c1ba602f04a
BytesCrafter <bytescrafter@gmail.com>
2020-07-27 12:31:54 +0800

    Added initial git files and VS class project.

---
+++ BaseClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TindaPress
+{
+    public class BaseClass
+    {
+        /// <summary>
+        /// Primary base domain url.
+        /// </summary>
+        public static string BaseDomainUrl
+        {
+            get
+            {
+                return "http://localhost";
+            }
+        }
+    }
+}

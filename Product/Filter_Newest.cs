c4c5e463db339ebd0c8ec492166690cedc85ef31
RusselJay27 <ponferrada.russel27@gmail.com>
2020-07-28 14:25:52 +0800

    Added Product Folder, Struck Folder, Token Class and Filter Newest Class. Added NewtonSoft.

---
+++ Product/Filter_Newest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using TindaPress.Product.Struct;
+
+namespace TindaPress.Product
+{
+    public class Filter_Newest
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Filter Newest Product Class.
+        /// </summary>
+        private static Filter_Newest instance;
+        public static Filter_Newest Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Filter_Newest();
+                return instance;
+            }
+        }
+        #endregion
+    }
+}

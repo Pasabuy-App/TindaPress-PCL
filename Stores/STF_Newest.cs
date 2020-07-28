f84f6cad70ae4a2208bc1cc9199b7260fd220316
RusselJay27 <ponferrada.russel27@gmail.com>
2020-07-28 14:55:16 +0800

    Added STF_Newest Class inside Store Folder.

---
+++ Stores/STF_Newest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using TindaPress.Product.Struct;
+
+namespace TindaPress.Product
+{
+    public class STF_Category
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Filter Newest Store Product Class.
+        /// </summary>
+        private static STF_Category instance;
+        public static STF_Category Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new STF_Category();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication for our Backend.
+        /// </summary>
+        HttpClient client;
+        public STF_Category()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Method
+        public async void GetData(string wp_id, string session_key, Action<bool, string> callback)
+        {
+            string getRequest = "?";
+            getRequest += "wpid=" + wp_id;
+            getRequest += "&snky=" + session_key;
+
+            var response = await client.GetAsync(BaseClass.BaseDomainUrl + "/datavice/api/v1/store/newest" + getRequest);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}

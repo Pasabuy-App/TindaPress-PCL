2a965c73735b5cb0635136cf0449e6637ff45db9
RusselJay27 <ponferrada.russel27@gmail.com>
2020-07-28 14:30:53 +0800

    Fixed the missing codes of PDF_Newest Class.

--- Product/Filter_Newest.cs
+++ Product/Filter_Newest.cs
@@ -24,5 +24,40 @@ namespace TindaPress.Product
             }
         }
         #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication for our Backend.
+        /// </summary>
+        HttpClient client;
+        public Filter_Newest()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Method
+        public async void GetData(string wp_id, string session_key, Action<bool, string> callback)
+        {
+            string getRequest = "?";
+            getRequest += "wpid=" + wp_id;
+            getRequest += "&snky=" + session_key;
+
+            var response = await client.GetAsync(BaseClass.BaseDomainUrl + "/datavice/api/v1/user/data" + getRequest);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
     }
 }

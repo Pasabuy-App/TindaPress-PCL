3993f7940304490ea7a4b6fd24c8ab66bf499706
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-20 12:33:39 +0800

    Added Delete Category.

---
+++ Category/Delete.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using TindaPress.Product.Struct;
+
+namespace TindaPress.Category
+{
+    public class Delete
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Delete Category Class.
+        /// </summary>
+        private static Delete instance;
+        public static Delete Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Delete();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public Delete()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Method
+        public async void Category(string wp_id, string session_key, string catid, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("catid", catid);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/category/delete", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}

ee1dafe1c2f6191b6306b453c3ee5d37adb5daef
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-20 14:13:46 +0800

    Added Activate Store.

---
+++ Stores/ActivateStore.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using TindaPress.Product.Struct;
+
+namespace TindaPress.Stores
+{
+    public class ActivateStore
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Activate Store Class.
+        /// </summary>
+        private static ActivateStore instance;
+        public static ActivateStore Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ActivateStore();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public ActivateStore()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Methodss
+        public async void Activate(string wp_id, string session_key, string stid, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("stid", stid);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/activate", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}

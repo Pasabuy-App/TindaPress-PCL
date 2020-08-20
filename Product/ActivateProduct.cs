c9cac47f302670b5bfe978643bf090e6b05c31c2
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-20 16:06:03 +0800

    Added Activate Product but need to fix in WP.

---
+++ Product/ActivateProduct.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using TindaPress.Product.Struct;
+
+namespace TindaPress.Product
+{
+    public class ActivateProduct
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Activate Product Class.
+        /// </summary>
+        private static ActivateProduct instance;
+        public static ActivateProduct Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ActivateProduct();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public ActivateProduct()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Methods
+        public async void Activate(string wp_id, string session_key, string pdid, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("pdid", pdid);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/products/activate", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}

a77a8912ea98b699b75dd89c171b73fd359cdba0
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-20 12:27:49 +0800

    Added Update Category.

---
+++ Category/Update.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using TindaPress.Product.Struct;
+
+namespace TindaPress.Category
+{
+    public class Update
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Update Category Class.
+        /// </summary>
+        private static Update instance;
+        public static Update Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Update();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public Update()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Method
+        public async void Category(string wp_id, string session_key, string catid, string title, string info, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("catid", catid);
+            dict.Add("title", title);
+            dict.Add("info", info);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/category/update", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}

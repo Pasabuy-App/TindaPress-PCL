667991647c4ee56741531522b5998b45b2b877d6
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-22 11:18:00 +0800

    Added Operations/OrderByID List by Operaion ID.

---
+++ Operations/OrderByID.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using TindaPress.Product.Struct;
+
+namespace TindaPress.Operations
+{
+    public class OrderByID
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of List ofOrders By Operations ID Class.
+        /// </summary>
+        private static OrderByID instance;
+        public static OrderByID Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new OrderByID();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public OrderByID()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Methods
+        public async void Listing(string wp_id, string session_key, string ops_id, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("ops_id", ops_id);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/operations/list/orders", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}

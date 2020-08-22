29f117fd482db566d340669841acd8197c1ad6cd
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-22 19:15:26 +0800

    Added StoreNear Me and InsertDoc but not working.

---
+++ Documents/InsertDoc.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using TindaPress.Product.Struct;
+
+namespace TindaPress.Documents
+{
+    public class InsertDoc
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Insert Document of Store Class.
+        /// </summary>
+        private static InsertDoc instance;
+        public static InsertDoc Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new InsertDoc();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public InsertDoc()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Methodss
+        public async void Insert(string wp_id, string session_key, string path, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("path", path);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/order/total/sales", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}
---
+++ Stores/StoreNear.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using TindaPress.Product.Struct;
+
+namespace TindaPress.Stores
+{
+    public class StoreNear
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Store Near Me Class.
+        /// </summary>
+        private static StoreNear instance;
+        public static StoreNear Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new StoreNear();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public StoreNear()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Methods
+        public async void NearMe(string wp_id, string session_key, string lat, string lon, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("lat", lat);
+            dict.Add("long", lon);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/nearme", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}

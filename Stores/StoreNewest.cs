484781c629c13e99502966b339f33a7105300063
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-22 19:20:44 +0800

    Added Store Newest.

---
+++ Stores/StoreNewest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using TindaPress.Product.Struct;
+
+namespace TindaPress.Stores
+{
+    public class StoreNewest
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Store Newest Class.
+        /// </summary>
+        private static StoreNewest instance;
+        public static StoreNewest Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new StoreNewest();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public StoreNewest()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Methods
+        public async void Newest(string wp_id, string session_key, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/newest", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}

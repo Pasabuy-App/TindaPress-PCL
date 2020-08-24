08be7a8cca02d1f1bcc7a67821e497bc0d53187d
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-24 15:22:22 +0800

    Added Listing of Variants.

---
+++ Variants/ListingVar.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using TindaPress.Product.Struct;
+
+namespace TindaPress.Variants
+{
+    public class ListingVar
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Listing of Variants by Parent ID, Product ID, Status, Variant ID and Type Class.
+        /// </summary>
+        private static ListingVar instance;
+        public static ListingVar Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ListingVar();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public ListingVar()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Methods
+        public async void Listing(string wp_id, string session_key, string pdid, string pid, string status, string vrid, string type, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("pdid", pdid);
+            dict.Add("pid", pid);
+            dict.Add("status", status);
+            dict.Add("vrid", vrid);
+            dict.Add("type", type);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/variants/list", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}

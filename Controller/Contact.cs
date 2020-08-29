dc4b2f086c7aa0c20c0c01b6eba61ca6194c7fdb
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-29 11:28:19 +0800

    Modified Address Class. Deleted Contact folder and added Contact Class under Controller folder.

--- Controller/Contact.cs
+++ Controller/Contact.cs
@@ -5,21 +5,21 @@ using Newtonsoft.Json;
 using System.Net.Http;
 using TindaPress.Controller.Struct;
 
-namespace TindaPress.Address
+namespace TindaPress.Controller
 {
-    public class Address
+    public class Contact
     {
         #region Fields
         /// <summary>
-        /// Instance of Address Class with Insert, Update, Delete, Activate and Listing Method.
+        /// Instance of  Contact Class with activate, delete, insert, listing and update method.
         /// </summary>
-        private static Address instance;
-        public static Address Instance
+        private static Contact instance;
+        public static Contact Instance
         {
             get
             {
                 if (instance == null)
-                    instance = new Address();
+                    instance = new Contact();
                 return instance;
             }
         }
@@ -30,95 +30,23 @@ namespace TindaPress.Address
         /// Web service for communication to our Backend.
         /// </summary>
         HttpClient client;
-        public Address()
+        public Contact()
         {
             client = new HttpClient();
         }
         #endregion
 
-        #region Listing Method
-        public async void Listing(string wp_id, string session_key, string stid, string type, string addr, string status, Action<bool, string> callback)
-        {
-            var dict = new Dictionary<string, string>();
-            dict.Add("wpid", wp_id);
-            dict.Add("snky", session_key);
-            if (stid != "") { dict.Add("stid", stid); }
-            if (type != "") { dict.Add("type", type); }
-            if (addr != "") { dict.Add("addr", addr); }
-            if (status != "") { dict.Add("status", status); }
-            var content = new FormUrlEncodedContent(dict);
-
-            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/address/list", content);
-            response.EnsureSuccessStatusCode();
-
-            if (response.IsSuccessStatusCode)
-            {
-                string result = await response.Content.ReadAsStringAsync();
-                Token token = JsonConvert.DeserializeObject<Token>(result);
-
-                bool success = token.status == "success" ? true : false;
-                string data = token.status == "success" ? result : token.message;
-                callback(success, data);
-            }
-            else
-            {
-                callback(false, "Network Error! Check your connection.");
-            }
-        }
-        #endregion
-
-        #region Insert Method
-        public async void Insert(string wp_id, string session_key, string stid, string type, string st, string bg, string ct, string pv, string co, string lat, string lon, Action<bool, string> callback)
+        #region Activate Method
+        public async void Activate(string wp_id, string session_key, string stid, string cid, Action<bool, string> callback)
         {
             var dict = new Dictionary<string, string>();
             dict.Add("wpid", wp_id);
             dict.Add("snky", session_key);
             dict.Add("stid", stid);
-            dict.Add("type", type);
-            dict.Add("st", st);
-            dict.Add("bg", bg);
-            dict.Add("ct", ct);
-            dict.Add("pv", pv);
-            dict.Add("co", co);
-            dict.Add("lat", lat);
-            dict.Add("long", lon);
+            dict.Add("cid", cid);
             var content = new FormUrlEncodedContent(dict);
 
-            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/address/insert", content);
-            response.EnsureSuccessStatusCode();
-
-            if (response.IsSuccessStatusCode)
-            {
-                string result = await response.Content.ReadAsStringAsync();
-                Token token = JsonConvert.DeserializeObject<Token>(result);
-
-                bool success = token.status == "success" ? true : false;
-                string data = token.status == "success" ? result : token.message;
-                callback(success, data);
-            }
-            else
-            {
-                callback(false, "Network Error! Check your connection.");
-            }
-        }
-        #endregion
-
-        #region Update Method
-        public async void Update(string wp_id, string session_key, string stid, string addr, string st, string bg, string ct, string pv, string co, Action<bool, string> callback)
-        {
-            var dict = new Dictionary<string, string>();
-            dict.Add("wpid", wp_id);
-            dict.Add("snky", session_key);
-            dict.Add("stid", stid);
-            dict.Add("addr", addr);
-            dict.Add("st", st);
-            dict.Add("bg", bg);
-            dict.Add("ct", ct);
-            dict.Add("pv", pv);
-            dict.Add("co", co);
-            var content = new FormUrlEncodedContent(dict);
-
-            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/address/update", content);
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/contacts/activate", content);
             response.EnsureSuccessStatusCode();
 
             if (response.IsSuccessStatusCode)
@@ -138,15 +66,16 @@ namespace TindaPress.Address
         #endregion
 
         #region Delete Method
-        public async void Delete(string wp_id, string session_key, string addr, Action<bool, string> callback)
+        public async void Delete(string wp_id, string session_key, string stid, string cid, Action<bool, string> callback)
         {
             var dict = new Dictionary<string, string>();
             dict.Add("wpid", wp_id);
             dict.Add("snky", session_key);
-            dict.Add("addr", addr);
+            dict.Add("stid", stid);
+            dict.Add("cid", cid);
             var content = new FormUrlEncodedContent(dict);
 
-            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/address/delete", content);
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/contacts/delete", content);
             response.EnsureSuccessStatusCode();
 
             if (response.IsSuccessStatusCode)
@@ -165,16 +94,18 @@ namespace TindaPress.Address
         }
         #endregion
 
-        #region Activate Method
-        public async void Activate(string wp_id, string session_key, string addr, Action<bool, string> callback)
+        #region Insert Method
+        public async void Insert(string wp_id, string session_key, string stid, string phone, string email, Action<bool, string> callback)
         {
             var dict = new Dictionary<string, string>();
             dict.Add("wpid", wp_id);
             dict.Add("snky", session_key);
-            dict.Add("addr", addr);
+            dict.Add("stid", stid);
+            dict.Add("phone", phone);
+            dict.Add("email", email);
             var content = new FormUrlEncodedContent(dict);
 
-            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/address/activate", content);
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/contacts/insert", content);
             response.EnsureSuccessStatusCode();
 
             if (response.IsSuccessStatusCode)
@@ -193,5 +124,66 @@ namespace TindaPress.Address
         }
         #endregion
 
+        #region Listing Method
+        public async void Listing(string wp_id, string session_key, string stid, string type, string cid, string status, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("stid", stid);
+            if (type != "") { dict.Add("type", type); }
+            if (cid != "") { dict.Add("cid", cid); }
+            if (status != "") { dict.Add("status", status); }
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/contacts/list", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+
+        #region Update Method
+        public async void Update(string wp_id, string session_key, string stid, string cid, string type, string val, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("stid", stid);
+            dict.Add("cid", cid);
+            dict.Add("type", type);
+            dict.Add("val", val);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/contacts/update", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
     }
 }

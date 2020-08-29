64436d3f7f990ba61f034fa5b0c270fac311b450
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-29 11:38:27 +0800

    Modified address and added document class. Deleted Documents folder.

--- Controller/Document.cs
+++ Controller/Document.cs
@@ -4,22 +4,23 @@ using System.Text;
 using Newtonsoft.Json;
 using System.Net.Http;
 using TindaPress.Controller.Struct;
+using System.IO;
 
-namespace TindaPress.Address
+namespace TindaPress.Controller
 {
-    public class Address
+    public class Document
     {
         #region Fields
         /// <summary>
-        /// Instance of Address Class with Insert, Update, Delete, Activate and Listing Method.
+        /// Instance of Documents Class with approve, delete, insert, listing and update metohd.
         /// </summary>
-        private static Address instance;
-        public static Address Instance
+        private static Document instance;
+        public static Document Instance
         {
             get
             {
                 if (instance == null)
-                    instance = new Address();
+                    instance = new Document();
                 return instance;
             }
         }
@@ -30,96 +31,23 @@ namespace TindaPress.Address
         /// Web service for communication to our Backend.
         /// </summary>
         HttpClient client;
-        public Address()
+        public Document()
         {
             client = new HttpClient();
         }
         #endregion
 
-        #region Listing Method
-        public async void Listing(string wp_id, string session_key, string stid, string type, string addr, string status, Action<bool, string> callback)
-        {
-            var dict = new Dictionary<string, string>();
-            dict.Add("wpid", wp_id);
-            dict.Add("snky", session_key);
-            if (stid != "") { dict.Add("stid", stid); }
-            if (type != "") { dict.Add("type", type); }
-            if (addr != "") { dict.Add("addr", addr); }
-            if (status != "") { dict.Add("status", status); }
-            var content = new FormUrlEncodedContent(dict);
-
-            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/address/list", content);
-            response.EnsureSuccessStatusCode();
-
-            if (response.IsSuccessStatusCode)
-            {
-                string result = await response.Content.ReadAsStringAsync();
-                Token token = JsonConvert.DeserializeObject<Token>(result);
-
-                bool success = token.status == "success" ? true : false;
-                string data = token.status == "success" ? result : token.message;
-                callback(success, data);
-            }
-            else
-            {
-                callback(false, "Network Error! Check your connection.");
-            }
-        }
-        #endregion
-
-        #region Insert Method
-        public async void Insert(string wp_id, string session_key, string stid, string type, string st, string bg, 
-                string ct, string pv, string co, string lat, string lon, Action<bool, string> callback)
+        #region Approve Method
+        public async void Approve(string wp_id, string session_key, string stid, string doc_id, Action<bool, string> callback)
         {
             var dict = new Dictionary<string, string>();
             dict.Add("wpid", wp_id);
             dict.Add("snky", session_key);
             dict.Add("stid", stid);
-            dict.Add("type", type);
-            dict.Add("st", st);
-            dict.Add("bg", bg);
-            dict.Add("ct", ct);
-            dict.Add("pv", pv);
-            dict.Add("co", co);
-            dict.Add("lat", lat);
-            dict.Add("long", lon);
+            dict.Add("doc_id", doc_id);
             var content = new FormUrlEncodedContent(dict);
 
-            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/address/insert", content);
-            response.EnsureSuccessStatusCode();
-
-            if (response.IsSuccessStatusCode)
-            {
-                string result = await response.Content.ReadAsStringAsync();
-                Token token = JsonConvert.DeserializeObject<Token>(result);
-
-                bool success = token.status == "success" ? true : false;
-                string data = token.status == "success" ? result : token.message;
-                callback(success, data);
-            }
-            else
-            {
-                callback(false, "Network Error! Check your connection.");
-            }
-        }
-        #endregion
-
-        #region Update Method
-        public async void Update(string wp_id, string session_key, string stid, string addr, string st, string bg, string ct, string pv, string co, Action<bool, string> callback)
-        {
-            var dict = new Dictionary<string, string>();
-            dict.Add("wpid", wp_id);
-            dict.Add("snky", session_key);
-            dict.Add("stid", stid);
-            dict.Add("addr", addr);
-            dict.Add("st", st);
-            dict.Add("bg", bg);
-            dict.Add("ct", ct);
-            dict.Add("pv", pv);
-            dict.Add("co", co);
-            var content = new FormUrlEncodedContent(dict);
-
-            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/address/update", content);
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/documents/approve", content);
             response.EnsureSuccessStatusCode();
 
             if (response.IsSuccessStatusCode)
@@ -139,15 +67,16 @@ namespace TindaPress.Address
         #endregion
 
         #region Delete Method
-        public async void Delete(string wp_id, string session_key, string addr, Action<bool, string> callback)
+        public async void Delete(string wp_id, string session_key, string stid, string doc_id, Action<bool, string> callback)
         {
             var dict = new Dictionary<string, string>();
             dict.Add("wpid", wp_id);
             dict.Add("snky", session_key);
-            dict.Add("addr", addr);
+            dict.Add("stid", stid);
+            dict.Add("doc_id", doc_id);
             var content = new FormUrlEncodedContent(dict);
 
-            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/address/delete", content);
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/documents/delete", content);
             response.EnsureSuccessStatusCode();
 
             if (response.IsSuccessStatusCode)
@@ -166,16 +95,82 @@ namespace TindaPress.Address
         }
         #endregion
 
-        #region Activate Method
-        public async void Activate(string wp_id, string session_key, string addr, Action<bool, string> callback)
+        #region Insert Method
+        public async void Insert(string wp_id, string session_key, string stid, string doc_type, string img, Action<bool, string> callback)
+        {
+            var multiForm = new MultipartFormDataContent();
+
+            multiForm.Add(new StringContent(wp_id), "wpid");
+            multiForm.Add(new StringContent(session_key), "snky");
+            multiForm.Add(new StringContent(stid), "stid");
+            multiForm.Add(new StringContent(doc_type), "doc_type");
+            FileStream fs = File.OpenRead(img);
+            multiForm.Add(new StreamContent(fs), "img", Path.GetFileName(img));
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/documents/insert", multiForm);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+
+        #region Listing Method
+        public async void Listing(string wp_id, string session_key, string stid, string doc_id, string doc_type, string status, Action<bool, string> callback)
         {
             var dict = new Dictionary<string, string>();
             dict.Add("wpid", wp_id);
             dict.Add("snky", session_key);
-            dict.Add("addr", addr);
+            dict.Add("stid", stid);
+            dict.Add("doc_id", doc_id);
+            dict.Add("doc_type", doc_type);
+            dict.Add("status", status);
             var content = new FormUrlEncodedContent(dict);
 
-            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/stores/address/activate", content);
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/documents/listing", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+
+        #region Update Method
+        public async void Update(string wp_id, string session_key, string stid, string doc_id, string doc_type, string img, Action<bool, string> callback)
+        {
+            var multiForm = new MultipartFormDataContent();
+
+            multiForm.Add(new StringContent(wp_id), "wpid");
+            multiForm.Add(new StringContent(session_key), "snky");
+            multiForm.Add(new StringContent(stid), "stid");
+            multiForm.Add(new StringContent(doc_id), "doc_id");
+            multiForm.Add(new StringContent(doc_type), "doc_type");
+            FileStream fs = File.OpenRead(img);
+            multiForm.Add(new StreamContent(fs), "img", Path.GetFileName(img));
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/tindapress/v1/documents/update", multiForm);
             response.EnsureSuccessStatusCode();
 
             if (response.IsSuccessStatusCode)
